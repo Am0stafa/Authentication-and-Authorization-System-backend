508285426fb40b7489ddf50604dae27d0b6eb8bb
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2024-11-13 00:04:51 +0200

    clean up

--- config/redisConnect.js
+++
@@ -1,9 +0,0 @@
-const Redis = require('ioredis');
-
-const redis = new Redis(`rediss://default:${process.env.REDIS}@eu2-proper-krill-31883.upstash.io:31883`);
-
-redis.on('error', (err) => {
-  console.log('Redis error: ' + err);
-} );
-
-module.exports = redis;
--- routes/register.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handelNewUser } = require('../controllers/registerController');
-const { upload } = require('../controllers/registerController'); // Assuming you export it from there
-
-router.post('/', upload.single('profileImg'), handelNewUser);
-
-module.exports = router;
